769fa71b03e220284a87151c53188a36d7add178
Mika Tammi <mikatammi@gmail.com>
2013-03-14 19:58:35 +0200

    Added uninstall to build run script

--- buildrun.sh
+++ buildrun.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+adb uninstall fi.ponr.motionmagnifier &
 ant debug &&
 ant debug install &&
 adb shell am start -a android.intent.action.MAIN -n fi.ponr.motionmagnifier/fi.ponr.motionmagnifier.ViewerActivity
